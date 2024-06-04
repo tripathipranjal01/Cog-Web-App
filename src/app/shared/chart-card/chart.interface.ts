c0ce6ad45f4bb5d41f1c663a80b4ff33ee8be486
suraj kumar jena <suraj.jena@cognecto.com>
2024-06-05 02:43:35 +0530

    CMD-508: fix(UI) 1) icon of user changed in header 2) icons changed in actionbar for live feed and hide show div 3) size of aside div changed in grid.scss 4) added some charts for reference 5) removed graph from chart card 6) modified chart card component as per requirement

--- src/app/shared/chart-card/chart.interface.ts
+++ src/app/shared/chart-card/chart.interface.ts
@@ -2,7 +2,6 @@ export interface IChartData {
   icon: string;
   heading: string;
   subHeading: string;
-  chartData: Array<Array<unknown>>;
   totalValue: string;
   precentage: string;
   currentMonth: string;
