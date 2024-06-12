9395d0eb337fffd7dd544093540e6194b0d68cb1
Lendup Lepcha <lendup.lepcha@cognecto.com>
2024-06-12 10:52:59 +0530

    CD-488: feature(refuel)- routing all refuel compunents

--- src/app/fuel/fuel-routing.module.ts
+++ src/app/fuel/fuel-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FuelComponent } from './fuel/fuel.component';
 import { HomeFuelComponent } from './home-fuel/home-fuel.component';
+import { RefuelAssetComponent } from './refuel/refuel-asset/refuel-asset.component';
+import { RefuelTankComponent } from './refuel/refuel-tank/refuel-tank.component';
+import { RefuelTankTypes } from './constants';
 
 export const FUEL_ROUTES_NAMES = {
   HOME: 'home',
+  REFUEL_ASSET: 'refuel-asset',
+  REFUEL_MAIN_TANK: 'refuel-main-tank',
+  REFUEL_BOWSER: 'refuel-bowser',
+  PLANT_BOOKING: 'plant-booking',
 };
 
 const FUEL_ROUTES = [
@@ -16,6 +23,37 @@ const FUEL_ROUTES = [
         path: '',
         component: FuelComponent,
         data: { breadcrumb: { alias: 'Fuel' } },
+      },
+      {
+        path: FUEL_ROUTES_NAMES.REFUEL_ASSET,
+        component: RefuelAssetComponent,
+        data: {
+          breadcrumb: { alias: 'Refuel Asset' },
+        },
+      },
+      {
+        path: FUEL_ROUTES_NAMES.REFUEL_MAIN_TANK,
+        component: RefuelTankComponent,
+        data: {
+          breadcrumb: { alias: 'Refuel Main Tank' },
+          operationType: RefuelTankTypes.MAIN_TANK,
+        },
+      },
+      {
+        path: FUEL_ROUTES_NAMES.REFUEL_BOWSER,
+        component: RefuelTankComponent,
+        data: {
+          breadcrumb: { alias: 'Refuel Bowser' },
+          operationType: RefuelTankTypes.BOWSER,
+        },
+      },
+      {
+        path: FUEL_ROUTES_NAMES.PLANT_BOOKING,
+        component: RefuelTankComponent,
+        data: {
+          breadcrumb: { alias: 'Plant Booking' },
+          operationType: RefuelTankTypes.BOOKING,
+        },
       },
     ],
   },
