d29f53ce6d58e678181f262163762a3e9b7c2302
Lendup Lepcha <lendup.lepcha@cognecto.com>
2024-06-12 10:40:42 +0530

    CD-488: feature(refuel)- add constants file in fuel directory

---
+++ src/app/fuel/constants.ts
@@ -0,0 +1,18 @@
+export const RefuelTankTypes = {
+  MAIN_TANK: 'Refuel Main Tank',
+  BOWSER: 'Refuel Bowser',
+  BOOKING: 'Plant Booking',
+};
+
+export const SuccessMessage = {
+  FUEL_SOURCE_UPDATED: 'Fuel source updated successfully.',
+  REFUEL_RECORD_SAVED: 'Refuel record saved successfully',
+};
+
+export const ErrorMessage = {
+  INVALID_DATA: 'Invalid data entered!!!',
+  INVALID_TANK_METER_READING: 'Invalid tank meter readings entered!!!',
+  NO_CONTENT: 'No content entered!!!',
+};
+
+export const TimeFormat_YYYY_MM_DD_HH_MM = 'yyyy-MM-DD HH:mm';
