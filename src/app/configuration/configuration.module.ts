700552a3408e774df2c4e53eeb5875cee150ab10
Lendup Lepcha <lendup.lepcha@cognecto.com>
2024-06-18 14:49:14 +0530

    CD-534:feature(config-home) - add store and service in configuration module

--- src/app/configuration/configuration.module.ts
+++ src/app/configuration/configuration.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigurationRoutingModule } from './configuration-routing.module';
 import { ConfigurationComponent } from './configuration/configuration.component';
+import { SharedModule } from '../shared/shared.module';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  CONFIGURATION_STATE_NAME,
+  ConfigurationEffects,
+  configurationReducer,
+} from './store';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [ConfigurationComponent],
-  imports: [CommonModule, ConfigurationRoutingModule],
+  imports: [
+    CommonModule,
+    ConfigurationRoutingModule,
+    SharedModule,
+    EffectsModule.forFeature([ConfigurationEffects]),
+    StoreModule.forFeature(CONFIGURATION_STATE_NAME, configurationReducer),
+  ],
 })
 export class ConfigurationModule {}
---
+++ src/app/configuration/store/index.ts
@@ -0,0 +1,4 @@
+export * from './configuration.action';
+export * from './configuration.reducer';
+export * from './configuration.effects';
+export * from './configuration.selector';
