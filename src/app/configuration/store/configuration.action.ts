dc932e08fe7acef3a4e0077353b0e8009da4788a
Rakesh Singh <rakesh.singh@cognecto.com>
2024-07-11 13:10:20 +0530

    CD-566: removed state from configuration

--- src/app/configuration/store/configuration.action.ts
+++
@@ -1,8 +0,0 @@
-import { createAction } from '@ngrx/store';
-
-const RESET_CONFIGURATION_MESSAGE_STATUS =
-  '[Configuration] Reset message status';
-
-export const resetMessageStatus = createAction(
-  RESET_CONFIGURATION_MESSAGE_STATUS
-);
